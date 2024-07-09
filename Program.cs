fd0662240bfadba8911e897ced817f023da1a525
FadyeEmad <114029125+FadyeEmad@users.noreply.github.com>
2024-07-09 07:56:28 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,135 +1,56 @@
-﻿namespace g1
-{
- #region Q1
-    public enum WeekDays
-    {
-        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
-    }
-    class prog1
-    {
-        static void Main(String[] args)
-        {
-            for (int i = 0; i < 7; i++)
-            {
-                WeekDays day = (WeekDays)i;
-                Console.WriteLine(day);
-            }
-        }
-    }
+//-------------------DEMO------------------
 
-#endregion
-
-
- #region Q2
-    public enum Season
-    {
-        Spring = 1, Summer = 2, Autumn = 3, Winter = 4
-    }
-    class prog2
-    {
-        static void Main(String[] args)
-        {
-            string s1 = Console.ReadLine();
-
-            bool flag = Enum.TryParse<Season>(s1, true, out Season s);
-
-            if (s == Season.Spring)
-            {
-                Console.WriteLine(" months is -> march , april , may ");
-            }
-            else if (s == Season.Summer)
-            {
-                Console.WriteLine(" months is -> June , July , August ");
-            }
-            else if (s == Season.Autumn)
-            {
-                Console.WriteLine(" months is -> September , October , November  ");
-            }
-            else if (s == Season.Winter)
-            {
-                Console.WriteLine(" months is -> December , January , February ");
-            }
-
-        }
-    }
-
-#endregion
-
-
-
-
-#region Q3
-    public enum WeekDays
-    {
-
-    }
-    class prog3
-    {
-        static void Main(String[] args)
-        {
-
-        }
-    }
-
-#endregion
-
-
-
-#region Q4
-    public enum colors
-    {
-        Red, Green, Blue
-    }
-    class prog4
-    {
-        static void Main(String[] args)
-        {
-            string color = Console.ReadLine();
-            Enum.TryParse<colors>(color, true, out colors c1);
-            if (c1 == colors.Red || c1 == colors.Blue || c1 == colors.Green)
-            {
-                Console.WriteLine("color is prime");
-            }
-            else
-            {
-                Console.WriteLine("color is not prime");
-            }
-        }
-    }
-
-#endregion
-
-#region Q6
-internal struct point
+namespace s1
 {
 
-    public int x, y;
-    public point(int s, int m)
+    internal class pro1
     {
-        x = 0;
+
+        static void Main(string[] args)
+        {
+            // ----------------- Exception --------------
+            //try
+            //    {
+            //        int m, o, k;
+            //        o = int.Parse(Console.ReadLine());
+            //        m = int.Parse(Console.ReadLine());
+            //        k = o / m;
+            //        int[] num = [1, 3, 2, 5];
+            //        num[10] = 50;
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+            //}
+            //int y;
+            //bool flag = int.TryParse(Console.ReadLine(), out y);
+            //Console.WriteLine( $" {flag} = {y}" );
+            //Class2 ahmed = new Class2();
+            //            Enum.TryParse(typeof(gender), Console.ReadLine(), out object x);
+            //            Console.WriteLine(x);   
+            //    }
+
+            //    }
+            //    //-------------- ENUM -------------
+            //    public enum gender
+            //    {
+            //        Male, Female
+            //    }
+            //    public enum WeekDays
+            ////    {
+            ////        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+            ////    }
+//            -------------- struct -------------
+//internal struct Point
+//        {
+//            public int X;
+//            public int Y;
+//            public Point(int _x, int _y)
+//            {
+//                X = _x;
+//                Y = _y;
+//            }
+//        }
     }
 }
-class prog6
-{
-    static void Main(String[] args)
-    {
-        point p1;
-        p1.x = int.Parse(Console.ReadLine());
-        p1.y = int.Parse(Console.ReadLine());
-        if (p1.x > p1.y)
-        {
-            Console.WriteLine($"DISTANCE = {p1.x - p1.y}");
-        }
-        else
-        {
-            Console.WriteLine($"DISTANCE = {p1.y - p1.x}");
-        }
-    }
-
-
-        }
-
-#endregion
-
-}
-//..
